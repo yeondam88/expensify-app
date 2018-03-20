2c9efc7c109bacadfbffbb6ce978c7ea792aeacf
Yeondam Park <giyen9088@gmail.com>
2018-03-19 23:05:19 -0700

    adding reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -11,6 +11,27 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
   }
 };
 
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
 const demoState = {
   expenses: [
     {
