efd8b0a81d587b14069b76bd3b35414ed08c5305
Yeondam Park <giyen9088@gmail.com>
2018-03-26 00:10:26 -0700

    wiring up with redux

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,18 +3,14 @@ import moment from "moment";
 import { SingleDatePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 
-// const date = new Date();
-
-const now = moment();
-console.log(now.format("MMM Do, YYYY"));
-
 class ExpenseForm extends React.Component {
   state = {
     description: "",
     note: "",
     amount: "",
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ""
   };
 
   onDescriptionChange = event => {
@@ -29,23 +25,45 @@ class ExpenseForm extends React.Component {
 
   onAmountChange = event => {
     const amount = event.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
 
   onDateChange = createdAt => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   };
 
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
   };
 
+  onSubmit = event => {
+    event.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({
+        error: "Please provide description and amount."
+      }));
+    } else {
+      console.log("Submitted!");
+      this.setState(() => ({ error: "" }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  };
+
   render() {
     return (
       <div>
-        <form>
+        {this.state.error ? <p>{this.state.error}</p> : ""}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
