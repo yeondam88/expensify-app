d16bd5f5d6d744f204957d10e1a60d937f9c5b1f
Yeondam Park <giyen9088@gmail.com>
2018-04-05 23:04:48 -0700

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running at port 3000");
+app.listen(port, () => {
+  console.log("Server is up!");
 });
