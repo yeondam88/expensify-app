4a9a808fed89fc0bb19799ae25c662352211f20a
Yeondam Park <giyen9088@gmail.com>
2018-04-08 20:47:58 -0700

    connecting firebase database

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyD3lLVhbeyoEClnP0GB0pSaEx81ZbjpFDA",
+  authDomain: "expensify-70afb.firebaseapp.com",
+  databaseURL: "https://expensify-70afb.firebaseio.com",
+  projectId: "expensify-70afb",
+  storageBucket: "expensify-70afb.appspot.com",
+  messagingSenderId: "53128478433"
+};
+
+firebase.initializeApp(config);
