cb94d720a890b17c90b23ae1e74fcda042a94952
Yeondam Park <giyen9088@gmail.com>
2018-04-09 21:19:59 -0700

    working on firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,3 +10,19 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: "Lloyd Park",
+  age: 31,
+  isMarried: true,
+  location: {
+    state: "CA",
+    city: "Anaheim"
+  }
+});
+
+database.ref().set({
+  age: 30
+});
