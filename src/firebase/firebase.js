ac99003007adcf5018c832cdaab7ba8ccc5e894b
Yeondam Park <giyen9088@gmail.com>
2018-04-12 22:21:13 -0700

    connecting firebase with redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,22 +13,4 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: "Lloyd Park",
-  age: 31,
-  isMarried: true,
-  location: {
-    state: "CA",
-    city: "Anaheim"
-  }
-});
-
-database.ref().set({
-  age: 30
-});
-
-database
-  .ref("age")
-  .remove()
-  .then(() => console.log("Data is removed"))
-  .catch(e => console.log(e));
+export { firebase, database as default };
