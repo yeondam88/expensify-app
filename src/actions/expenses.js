e6e0f6c76cd653238c604ea98fb836a02117c0b9
Yeondam Park <giyen9088@gmail.com>
2018-04-14 16:12:35 -0700

    Fetching data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -40,3 +40,12 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const setExpenses = expenses => ({
+  type: "SET_EXPENSES",
+  expenses
+});
+
+export const startSetExpenses = () => ({
+  type: "START_SET_EXPENSES"
+});
