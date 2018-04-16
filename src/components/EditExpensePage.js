c1651c09de0d652a60913e3e163fdacca9e91c0b
Yeondam Park <giyen9088@gmail.com>
2018-04-15 21:23:41 -0700

    convert editpensePage to Class based component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 
 import { editExpense, removeExpense } from "../actions/expenses";
 
-const EditExpensePage = props => (
-  <div>
-    <ExpenseForm
-      expense={props.expense}
-      onSubmit={expense => {
-        props.dispatch(editExpense(props.expense.id, expense));
-        props.history.push("/");
-        console.log("updated", expense);
-      }}
-    />
-    <button
-      onClick={() => {
-        props.dispatch(removeExpense({ id: props.expense.id }));
-        props.history.push("/");
-      }}
-    >
-      Remove
-    </button>
-  </div>
-);
+class EditExpensePage extends Component {
+  onSubmit = expense => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push("/");
+  };
+
+  onRemove = () => {
+    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onRemove}>Remove</button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
