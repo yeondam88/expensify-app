063d682bab2f17a9fb64f283e60030c5c056e894
Yeondam Park <giyen9088@gmail.com>
2018-04-15 21:19:44 -0700

    removing expenses from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -10,11 +10,16 @@ export const startAddExpense = (expenseData = {}) => {
   return dispatch => {
     const {
       description = "",
-      note = "",
-      amount = 0,
-      createdAt = 0
+        note = "",
+        amount = 0,
+        createdAt = 0
     } = expenseData;
-    const expense = { description, note, amount, createdAt };
+    const expense = {
+      description,
+      note,
+      amount,
+      createdAt
+    };
 
     database
       .ref("expenses")
@@ -30,10 +35,24 @@ export const startAddExpense = (expenseData = {}) => {
   };
 };
 
-export const removeExpense = ({ id } = {}) => ({
+export const removeExpense = ({
+  id
+} = {}) => ({
   type: "REMOVE_EXPENSE",
   id
 });
+
+export const startRemoveExpense = ({
+  id
+} = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({
+        id
+      }));
+    })
+  }
+}
 
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
