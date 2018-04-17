522549329097ffc3fa31baefdf1d421913870c64
Yeondam Park <giyen9088@gmail.com>
2018-04-16 18:12:46 -0700

    Converting to class based component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,24 +4,28 @@ import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 import { removeExpense } from "../actions/expenses";
 
-const ExpenseList = props => (
-  <div>
-    <h1>Expense List</h1>
-    {props.expenses.length ? (
-      props.expenses.map(expense => {
-        return (
-          <ExpenseListItem
-            key={expense.id}
-            {...expense}
-            remove={props.remove}
-          />
-        );
-      })
-    ) : (
-      <p>No expenses..</p>
-    )}
-  </div>
-);
+class ExpenseList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Expense List</h1>
+        {this.props.expenses.length ? (
+          this.props.expenses.map(expense => {
+            return (
+              <ExpenseListItem
+                key={expense.id}
+                {...expense}
+                remove={this.props.remove}
+              />
+            );
+          })
+        ) : (
+          <p>No expenses..</p>
+        )}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
