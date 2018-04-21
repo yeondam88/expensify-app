c6ea7b5c6d9644c064e1411f21e6f3ee83c5271d
Yeondam Park <giyen9088@gmail.com>
2018-04-21 12:59:07 -0700

    revert yesterday work

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,18 +3,10 @@ import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
 class LoginPage extends Component {
-  state = {
-    login: false
-  };
-
   render() {
     return (
       <div>
-        {this.state.login ? (
-          <button onClick={this.props.startLogin}>Login</button>
-        ) : (
-          <button onClick={this.props.startLogin}>Logout</button>
-        )}
+        <button onClick={this.props.startLogin}>Login</button>
       </div>
     );
   }
