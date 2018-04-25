ab7963be270233e6f422cc43b5bacbfb3d9559a7
Yeondam Park <giyen9088@gmail.com>
2018-04-24 22:25:31 -0700

    styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,14 @@ import { startLogin } from "../actions/auth";
 class LoginPage extends Component {
   render() {
     return (
-      <div>
-        <button onClick={this.props.startLogin}>Login</button>
+      <div className="box-layout">
+        <div className="box-layout__box">
+          <h1 className="box-layout__title">Expensify App</h1>
+          <p>It's time to get your expenses under control.</p>
+          <button className="box-layout__btn" onClick={this.props.startLogin}>
+            Login with Google
+          </button>
+        </div>
       </div>
     );
   }
