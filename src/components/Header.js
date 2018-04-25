cda400439ea9cb33849e590c6bd7cf5f778c6c49
Yeondam Park <giyen9088@gmail.com>
2018-04-24 21:01:42 -0700

    Header component route name changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,14 @@ import { startLogout } from "../actions/auth";
 const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink exact to="/" activeClassName="is-active">
-      Go Home
+    <NavLink exact to="/dashboard" activeClassName="is-active">
+      Dashboard
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
       Create
     </NavLink>
     <NavLink to="/edit" activeClassName="is-active">
       Edit
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
     </NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
