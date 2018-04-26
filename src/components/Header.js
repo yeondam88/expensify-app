e60930507fed83a2d399bc64c23d6222b30fe2d2
Yeondam Park <giyen9088@gmail.com>
2018-04-25 22:20:56 -0700

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,9 @@ const Header = ({ startLogout }) => (
         <Link className="header__title" exact to="/dashboard">
           <h1>Expensify</h1>
         </Link>
-        <button onClick={startLogout}>Logout</button>
+        <button className="button button--link" onClick={startLogout}>
+          Logout
+        </button>
       </div>
     </div>
   </header>
