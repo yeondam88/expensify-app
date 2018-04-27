cf17968cf6514e3f5f9c8e834afe1d1ed49ea574
Yeondam Park <giyen9088@gmail.com>
2018-04-26 22:58:39 -0700

    adding loader

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { login, logout } from "./actions/auth";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 
@@ -25,7 +26,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
